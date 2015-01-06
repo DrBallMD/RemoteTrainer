9f1059e02d6d4b1e73e4926e71744747a96c68f2
bryzgalov_es <cloudfactory@gmx.com>
2015-01-06 18:27:09 +0600

    потерял чутка

---
+++ WcfService1/IService1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени интерфейса "IService1" в коде и файле конфигурации.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        
+        [OperationContract]
+        List<AISdb.AISTask> getTasks();
+        [OperationContract]
+        ANeuralNetwork.ANetwork getNeuralNetwork(int id);
+        [OperationContract]
+        void setNetworkResults(int id, ANeuralNetwork.ANetwork net, float newError);
+        [OperationContract]
+        float getError(int id);
+        [OperationContract]
+        List<ANeuralNetwork.StudyData> getStudyData(int id);
+        [OperationContract]
+        void addStudyData(int id, List<ANeuralNetwork.StudyData> data);
+        [OperationContract]
+        void addTask(AISdb.AISTask tt, List<string> parameters);
+        [OperationContract]
+        void updateTask(AISdb.AISTask tt);
+        [OperationContract]
+        void deleteTask(int id);
+        // TODO: Добавьте здесь операции служб
+    }
+
+
+    // Используйте контракт данных, как показано в примере ниже, чтобы добавить составные типы к операциям служб.
+    
+}
