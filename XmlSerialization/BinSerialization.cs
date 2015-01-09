5e4363a591e8e4ee52f9761297414f25d646e135
DrBallMD <test>
2015-01-09 23:56:38 +0700

    добавил библиотеку сериализации

---
+++ XmlSerialization/BinSerialization.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace GenXmlSerialization
+{
+    public static class BinSerialization
+    {
+        /// <summary>
+        /// Сохраняет объект в бинарный файл
+        /// </summary>
+        /// <typeparam name="T">Тип сохраняемого объекта</typeparam>
+        /// <param name="obj">Сохраняемый объект</param>
+        /// <param name="path">Путь к файлу</param>
+        public static void Serialize<T>(T obj, string path)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.None);
+            formatter.Serialize(stream, obj);
+            stream.Close();
+        }
+        /// <summary>
+        /// Восстанавливает объект из бинарного файла
+        /// </summary>
+        /// <typeparam name="T">Тип восстанавливаемого объекта</typeparam>
+        /// <param name="path">Путь к файлу</param>
+        /// <returns>Возвращает объект заданного типа</returns>
+        public static T Deserialize<T>(string path)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            Stream readStream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read);
+            T result = (T)formatter.Deserialize(readStream);
+            readStream.Close();
+            return result;
+        }
+    }
+}
---
+++ XmlSerialization/XmlSerialization.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace GenXmlSerialization
+{
+    public static class XmlSerialization
+    {
+        public static void Serialize<T>(T obj, string path)
+        {
+            XmlSerializer serializer = new XmlSerializer(obj.GetType());
+            TextWriter textWriter = new StreamWriter(path);
+            serializer.Serialize(textWriter, obj);
+            textWriter.Flush();
+            textWriter.Close();
+        }
+        public static void Serialize<T>(List<T> obj, string path)
+        {
+            XmlSerializer serializer = new XmlSerializer(obj.GetType());
+            TextWriter textWriter = new StreamWriter(path);
+            serializer.Serialize(textWriter, obj);
+            textWriter.Flush();
+            textWriter.Close();
+        }
+        public static List<T> DeserializeCollection<T>(string path)
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(List<T>));
+            TextReader textReader = new StreamReader(path);
+            List<T> serializedData;
+            serializedData = (List<T>)deserializer.Deserialize(textReader);
+            textReader.Close();
+            return serializedData;
+        }
+        public static T DeserializeObject<T>(string path)
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(T));
+            TextReader textReader = new StreamReader(path);
+            T serializedData;
+            serializedData = (T) deserializer.Deserialize(textReader);
+            textReader.Close();
+            return serializedData;
+        }
+    }
+}
